4e4fd64a9629151d3afdea4bf57fad8922d59417
fedorshm <147091881+fedorshm@users.noreply.github.com>
2024-10-16 14:55:02 +0700

    Update urls.py

--- analysis_tool/urls.py
+++ analysis_tool/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import analyze_text
 
 urlpatterns = [
-    path('analyze/', analyze_text),
+    path('analyze/', analyze_text, name='analyze_text'),
 ]
+     
