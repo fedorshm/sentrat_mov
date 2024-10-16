56d8a0aedfbabedc422173c74b93de018a042330
fedorshm <147091881+fedorshm@users.noreply.github.com>
2024-10-16 15:12:02 +0700

    Update views.py

--- analysis_tool/views.py
+++ analysis_tool/views.py
@@ -1,30 +1,48 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from transformers import pipeline
 
+sentiment_model = pipeline("text-classification", model="Gnider/roberta_dist_sent", truncation=True)
+rating_model = pipeline("text-classification", model="Gnider/roberta_dist_rat", truncation=True)
 
-sentiment_model = pipeline("text-classification", model="Gnider/roberta_dist_sent")
-rating_model = pipeline("text-classification", model="Gnider/roberta_dist_rat")
+def chunk_text(text, max_length=512):
+    return [text[i:i + max_length] for i in range(0, len(text), max_length)]
+
+def aggregate_results(sentiment_chunks, rating_chunks):
+    sentiment_scores = {'positive': 0, 'negative': 0}
+    for chunk in sentiment_chunks:
+        for score in chunk:
+            if score['label'] == 'LABEL_1':  
+                sentiment_scores['positive'] += score['score']
+            elif score['label'] == 'LABEL_0':
+                sentiment_scores['negative'] += score['score']
+
+    final_sentiment = 'positive' if sentiment_scores['positive'] > sentiment_scores['negative'] else 'negative'
+
+    rating_aggregate = {i: 0 for i in range(8)} 
+    label_to_rating = {0: 1, 1: 2, 2: 3, 3: 4, 4: 7, 5: 8, 6: 9, 7: 10}
+
+    for chunk in rating_chunks:
+        for score in chunk:
+            label = int(score['label'].split('_')[1])
+            rating_aggregate[label] += score['score']
+
+    sorted_ratings = sorted(rating_aggregate.items(), key=lambda item: item[1], reverse=True)[:3]
+    top_ratings = [{"rating": label_to_rating[label], "score": score} for label, score in sorted_ratings]
+
+    final_rating = label_to_rating[sorted_ratings[0][0]]
+
+    return sentiment_scores, final_sentiment, top_ratings, final_rating
 
 def analyze_text(request):
     if request.method == 'POST':
         text = request.POST.get('text', '')
 
-        sentiment_results = sentiment_model(text, return_all_scores=True)[0]
-        label_to_sentiment = {'LABEL_0': 'negative', 'LABEL_1': 'positive'}
-        sentiment_scores = {label_to_sentiment[res['label']]: res['score'] for res in sentiment_results}
+        text_chunks = chunk_text(text)
 
-        rating_results = rating_model(text, return_all_scores=True)[0]
-        sorted_ratings = sorted(rating_results, key=lambda x: x['score'], reverse=True)[:3]
-        label_to_rating = {0: 1, 1: 2, 2: 3, 3: 4, 4: 7, 5: 8, 6: 9, 7: 10}
-        top_ratings = [{"rating": label_to_rating[int(res['label'].split('_')[1])], "score": res['score']} for res in sorted_ratings]
+        sentiment_chunks = [sentiment_model(chunk, return_all_scores=True)[0] for chunk in text_chunks]
+        rating_chunks = [rating_model(chunk, return_all_scores=True)[0] for chunk in text_chunks]
 
-        if sentiment_scores['positive'] > sentiment_scores['negative']:
-            final_sentiment = "positive"
-        else:
-            final_sentiment = "negative"
-
-        final_rating = top_ratings[0]['rating']
+        sentiment_scores, final_sentiment, top_ratings, final_rating = aggregate_results(sentiment_chunks, rating_chunks)
 
         return render(request, 'analysis_tool/results.html', {
             'text': text,
@@ -33,6 +51,6 @@ def analyze_text(request):
             'final_sentiment': final_sentiment,
             'final_rating': final_rating
         })
-    
+
     return render(request, 'analysis_tool/analyze_text.html')
 
